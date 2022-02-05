1035c04e0d9f9d5981a1ed1dacd3f18f435562f4
xsl <xiaosailun@163.com>
2022-02-05 22:01:39 +0800

    webpack

---
+++ config/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'static/js/[name].[contenthash:8].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "public/index.html"
+        }),
+    ]
+}
