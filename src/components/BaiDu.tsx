a483645c0091177da41c245dc74005f9a27eec9a
xsl <xiaosailun@163.com>
2022-02-06 17:43:38 +0800

    logo暂用百度的

--- src/components/BaiDu.tsx
+++ src/components/BaiDu.tsx
@@ -6,7 +6,17 @@ console.log(789, styles);
 function Baidu() {
     return (
         <div className={styles.baidu}>
-            456
+            <div className={styles.logo}>
+                <img
+                    src="https://www.baidu.com/img/PC_880906d2a4ad95f5fafb2e540c5cdad7.png"
+                    alt="百度"
+                    width="270"
+                    height="129"
+                />
+            </div>
+            <div>
+
+            </div>
         </div>
     )
 }
