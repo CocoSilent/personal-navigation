0dde6f2d297a071e7254298d741a7d5523c593a9
xsl <xiaosailun@163.com>
2022-02-06 20:31:43 +0800

    删除无用css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <div style={{ height: '20px', backgroundColor: "#FFF5EE" }}>
+        <div style={{ height: '20px' }}>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <div style={{ height: '20px', backgroundColor: "#FFF5EE" }}>
+        <div style={{ height: '20px' }}>
         </div>
     )
 }
