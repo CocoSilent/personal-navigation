d74e04e4f34519121c2906d1b070643517bb1095
xsl <xiaosailun@163.com>
2022-02-06 20:41:02 +0800

    百度搜索

--- src/components/BaiDu.tsx
+++ src/components/BaiDu.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input, Button } from '@douyinfe/semi-ui';
 import { IconSearch } from '@douyinfe/semi-icons';
 import styles from './baidu.module.less';
 
 function Baidu() {
+    const [searchText, setSearchText] = useState('');
+    function search() {
+        if(!searchText) {
+            return;
+        }
+        window.open(`https://www.baidu.com/s?wd=${searchText}`);
+    }
     return (
         <div className={styles.baidu}>
             <div className={styles.logo}>
@@ -18,9 +25,12 @@ function Baidu() {
                 <Input
                     autofocus
                     className={styles.input}
-                    suffix={<Button type="primary" theme="light" icon={<IconSearch style={{ color:"#4e6ef2" }} />} aria-label="搜索"/>}
+                    suffix={<Button type="primary" theme="light" icon={<IconSearch style={{ color:"#4e6ef2" }} />} aria-label="搜索" onClick={search}/>}
                     showClear
                     size="large"
+                    value={searchText}
+                    onChange={setSearchText}
+                    onEnterPress={search}
                 ></Input>
             </div>
         </div>
