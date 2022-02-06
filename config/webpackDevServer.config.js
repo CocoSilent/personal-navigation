653b2a9bea43c31c3832db1fbedd583a9bb8ff53
xsl <xiaosailun@163.com>
2022-02-06 14:52:44 +0800

    解决public访问

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -7,7 +7,7 @@ module.exports = merge(config, {
     devtool: 'inline-source-map',
     devServer: {
         static: {
-            directory: path.join(__dirname, '../dist'),
+            directory: path.join(__dirname, '../public'),
         },
         compress: true,
         port: 9000,
