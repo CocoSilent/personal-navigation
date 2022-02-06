ac08470e6d5a469940d83fdae740e6cadb867bbd
xsl <xiaosailun@163.com>
2022-02-06 20:31:01 +0800

    百度搜索

--- src/components/BaiDu.tsx
+++ src/components/BaiDu.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Input } from '@douyinfe/semi-ui';
+import { Input, Button } from '@douyinfe/semi-ui';
 import { IconSearch } from '@douyinfe/semi-icons';
-import './baidu.less';
+import styles from './baidu.module.less';
 
 function Baidu() {
     return (
-        <div className="baidu">
-            <div className="logo">
+        <div className={styles.baidu}>
+            <div className={styles.logo}>
                 <img
                     src="https://www.baidu.com/img/PC_880906d2a4ad95f5fafb2e540c5cdad7.png"
                     alt="百度"
@@ -14,8 +14,14 @@ function Baidu() {
                     height="129"
                 />
             </div>
-            <div>
-                <Input suffix={<IconSearch />} showClear></Input>
+            <div className={styles.search}>
+                <Input
+                    autofocus
+                    className={styles.input}
+                    suffix={<Button type="primary" theme="light" icon={<IconSearch style={{ color:"#4e6ef2" }} />} aria-label="搜索"/>}
+                    showClear
+                    size="large"
+                ></Input>
             </div>
         </div>
     )
