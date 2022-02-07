7ec9f3b8eb1d1f539ae0a5335b27ea0d52e3c5bf
xsl <xiaosailun@163.com>
2022-02-07 15:04:03 +0800

    @cloudbase/js-sdk  接入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from '@douyinfe/semi-ui';
+import { getApp } from '../tcb'
+
+const app = getApp();
 
 function Header() {
+
+    const [callFunctionResult, setCallFunctionResult] = useState("");
+
+    const callFunction = async () => {
+        try {
+            const res = await app.callFunction({
+                name: "helloworld",
+                data: {
+                    foo: "bar",
+                },
+            });
+            setCallFunctionResult(JSON.stringify(res));
+        } catch (e: any) {
+            setCallFunctionResult(e.message);
+        }
+    };
+
     return (
         <div style={{ height: '20px' }}>
+            <Button onClick={callFunction}>调用云函数</Button>
+            <p>
+                <b>云函数执行结果</b>
+            </p>
+            <p>{callFunctionResult}</p>
         </div>
     )
 }
