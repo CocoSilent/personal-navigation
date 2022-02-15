b9dbcf415d66dd298d7cdfedf93b8d1c2ac3dc14
xsl <xiaosailun@163.com>
2022-02-15 22:44:25 +0800

    备案信息

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ import styles from './footer.module.less';
 function Footer() {
     return (
         <div className={styles.footer}>
-            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备2022004306号-1</a>
+            <div className={styles.wrap}>
+                <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备2022004306号-1</a>
+            </div>
         </div>
     )
 }
