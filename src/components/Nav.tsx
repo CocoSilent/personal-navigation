eff1a8966d034a32cdf6413c6b4003d7fb786354
xsl <xiaosailun@163.com>
2022-02-16 22:20:30 +0800

    修改导航

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,37 +1,44 @@
-import React from "react";
-import { IconEdit, IconDelete } from '@douyinfe/semi-icons';
+import React, {useState} from "react";
+import {IconEdit, IconDelete} from '@douyinfe/semi-icons';
 import styles from './nav.module.less';
+import {Tooltip} from "@douyinfe/semi-ui";
 
 const navs = [
     {
         favicon: 'https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/favicon-32x32.png',
         url: 'https://juejin.cn/',
         name: '掘金',
+        optionVisible: false,
     },
     {
         favicon: 'https://github.com/favicon.ico',
         url: 'https://github.com/',
         name: 'github',
+        optionVisible: false,
     },
     {
         favicon: 'https://static.zhihu.com/heifetz/favicon.ico',
         url: 'https://www.zhihu.com/',
         name: '知乎',
+        optionVisible: false,
     },
     {
         favicon: 'https://leetcode-cn.com/favicon.ico',
         url: 'https://leetcode-cn.com/',
         name: '力扣',
+        optionVisible: false,
     },
     {
         favicon: 'https://g.csdnimg.cn/static/logo/favicon32.ico',
         url: 'https://www.csdn.net/',
         name: 'CSDN',
+        optionVisible: false,
     },
     {
         favicon: 'https://g.csdnimg.cn/static/logo/favicon32.ico',
         url: 'https://www.csdn.net/',
         name: 'CSDN',
+        optionVisible: false,
     },
 ]
 
@@ -43,16 +50,30 @@ function Nav() {
                     navs.map((nav, index) => {
                         return (
                             <div key={index} className={styles.navItem}>
-                                <img width='32px' height='32px' src={nav.favicon}  onClick={() => {window.open(nav.url);}}/>
-                                <div onClick={() => {window.open(nav.url);}}>{nav.name}</div>
-                                <div className={styles.options}>
-                                    <div>
-                                        <IconEdit />
+                                <Tooltip className={styles.toolTip}
+                                    content={
+                                        <div className={styles.options}>
+                                            <div>
+                                                <IconEdit className={styles.icon} size="small"/>
+                                            </div>
+                                            <div>
+                                                <IconDelete className={styles.icon} size="small"/>
+                                            </div>
+                                        </div>
+                                    }
+                                    position="rightTop"
+                                >
+                                    <div
+                                        className={styles.content}
+                                    >
+                                        <img width='32px' height='32px' src={nav.favicon} onClick={() => {
+                                            window.open(nav.url);
+                                        }}/>
+                                        <div onClick={() => {
+                                            window.open(nav.url);
+                                        }}>{nav.name}</div>
                                     </div>
-                                    <div>
-                                        <IconDelete />
-                                    </div>
-                                </div>
+                                </Tooltip>
                             </div>)
                     })
                 }
