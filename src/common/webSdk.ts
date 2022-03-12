f9f6aa0a4023f89610a08ed374520c0b4f03c510
xsl <xiaosailun@163.com>
2022-03-12 11:24:20 +0800

    window变量问题

--- src/common/webSdk.ts
+++ src/common/webSdk.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
-const myCloud = new window.cloud.Cloud({
+const myCloud:any = new window.cloud.Cloud({
     appid: 'wx56f18e2958172116',
     // 必填，表示是未登录模式
     identityless: true,
@@ -11,7 +10,11 @@ const myCloud = new window.cloud.Cloud({
 
 // 跨账号调用，必须等待 init 完成
 // init 过程中，资源方小程序对应环境下的 cloudbase_auth 函数会被调用，并需返回协议字段（见下）来确认允许访问、访问时长以及可自定义安全规则
-myCloud.init();
+myCloud.init().then(() => {
+    window.myCloud = myCloud;
+});
+
+export {}
 
 
 
---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+declare interface Window {
+    cloud: any;
+    myCloud: any;
+}
