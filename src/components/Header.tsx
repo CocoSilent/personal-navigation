39856d9df4e779705d45c71942e854d518b17857
xsl <xiaosailun@163.com>
2022-03-12 12:36:04 +0800

    操作引导

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Tooltip } from '@douyinfe/semi-ui';
-import { IconGithubLogo, IconImage } from '@douyinfe/semi-icons';
+import { IconGithubLogo, IconHelpCircle, IconImage } from '@douyinfe/semi-icons';
 // import { getApp } from '../tcb'
 import { GITHUB_PROJECT } from '../common/constant';
 
@@ -34,6 +34,10 @@ function Header() {
                 {/*             style={{marginLeft: 8}}*/}
                 {/*             onClick={() => window.open(GITHUB_PROJECT)} />*/}
                 {/*</Tooltip>*/}
+                <Tooltip content="鼠标放到网站图标上停留1秒以上就可以新增、修改、删除网站了~">
+                    <Button  size="default" icon={<IconHelpCircle size="large" style={{ color: "black"}} />}
+                             style={{marginLeft: 8}} />
+                </Tooltip>
                 <Tooltip content="项目详情">
                     <Button  size="default" icon={<IconGithubLogo size="large" style={{ color: "black"}} />}
                             style={{marginLeft: 8}}
