056fc79ce79f7fb9ee54f81aaf4c38afd3344cdd
xsl <xiaosailun@163.com>
2022-03-12 11:27:52 +0800

    window变量问题

--- src/global.d.ts
+++
@@ -1,4 +0,0 @@
-declare interface Window {
-    cloud: any;
-    myCloud: any;
-}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,9 @@ declare module '*.module.less' {
     const classes: { readonly [key: string]: string };
     export default classes;
 }
+
+
+declare interface Window {
+    cloud: any;
+    myCloud: any;
+}
